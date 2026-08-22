5f9829a7403515e6c24ade12c68274ba98d0f2d0
Pedro Dumont <phdumont@Dumont-Mac.local>
2026-08-21 21:06:48 -0300

    bootstrap

---
+++ PedroDumont/Aulas/boostrap_js/script.js
@@ -0,0 +1,18 @@
+const cursos = [
+    {
+        id: 1,
+        nome: "HTML e CSS",
+        descricao: "Aprenda a estrutura",
+        cargaHoraria: 40,
+        ativo: true,
+        categoria: "Frontend"
+    },
+    {
+        id: 2,
+        nome: "javascript",
+        descricao: "Aprenda a programar",
+        cargaHoraria: 40,
+        ativo: true,
+        categoria: "Backend"
+    }
+];
